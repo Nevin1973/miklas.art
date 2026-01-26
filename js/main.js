e62680b0aeafc4494121c837fed431b0675f2de0
Mariusz Miklas <miklasmariusz@gmail.com>
2026-01-26 22:51:43 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,164 @@
+(function () {
+    'use strict';
+    
+    // Mobile 
+    var isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        }
+        , BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        }
+        , iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        }
+        , Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        }
+        , Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        }
+        , any: function () {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+    
+    // Sections background image
+    var pageSection = $(".bg-img, section");
+    pageSection.each(function(indx){
+        if ($(this).attr("data-background")){
+            $(this).css("background-image", "url(" + $(this).data("background") + ")");
+        }
+    });
+
+    // Animations
+    var contentWayPoint = function () {
+        var i = 0;
+        $('.animate-box').waypoint(function (direction) {
+            if (direction === 'down' && !$(this.element).hasClass('animated')) {
+                i++;
+                $(this.element).addClass('item-animate');
+                setTimeout(function () {
+                    $('body .animate-box.item-animate').each(function (k) {
+                        var el = $(this);
+                        setTimeout(function () {
+                            var effect = el.data('animate-effect');
+                            if (effect === 'fadeIn') {
+                                el.addClass('fadeIn animated');
+                            }
+                            else if (effect === 'fadeInLeft') {
+                                el.addClass('fadeInLeft animated');
+                            }
+                            else if (effect === 'fadeInRight') {
+                                el.addClass('fadeInRight animated');
+                            }
+                            else {
+                                el.addClass('fadeInUp animated');
+                            }
+                            el.removeClass('item-animate');
+                        }, k * 200, 'easeInOutExpo');
+                    });
+                }, 100);
+            }
+        }, {
+            offset: '85%'
+        });
+    };
+    
+    // Burger Menu 
+    var burgerMenu = function () {
+        $('.js-miklas-nav-toggle').on('click', function (event) {
+            event.preventDefault();
+            var $this = $(this);
+            if ($('body').hasClass('offcanvas')) {
+                $this.removeClass('active');
+                $('body').removeClass('offcanvas');
+            }
+            else {
+                $this.addClass('active');
+                $('body').addClass('offcanvas');
+            }
+        });
+    };
+    
+    // Click outside of offcanvass
+    var mobileMenuOutsideClick = function () {
+        $(document).click(function (e) {
+            var container = $("#miklas-aside, .js-miklas-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($('body').hasClass('offcanvas')) {
+                    $('body').removeClass('offcanvas');
+                    $('.js-miklas-nav-toggle').removeClass('active');
+                }
+            }
+        });
+        $(window).scroll(function () {
+            if ($('body').hasClass('offcanvas')) {
+                $('body').removeClass('offcanvas');
+                $('.js-miklas-nav-toggle').removeClass('active');
+            }
+        });
+    };
+
+    // Document on load.
+    $(function () {
+        contentWayPoint();
+        burgerMenu();
+        mobileMenuOutsideClick();
+    });
+    
+    // MagnificPopup
+    $(".img-zoom").magnificPopup({
+        type: "image"
+        , closeOnContentClick: !0
+        , mainClass: "mfp-fade"
+        , gallery: {
+            enabled: !0
+            , navigateByImgClick: !0
+            , preload: [0, 1]
+        }
+    })
+    $('.magnific-youtube, .magnific-vimeo, .magnific-custom').magnificPopup({
+        disableOn: 700
+        , type: 'iframe'
+        , mainClass: 'mfp-fade'
+        , removalDelay: 300
+        , preloader: false
+        , fixedContentPos: false
+    });
+    
+}());
+
+// Contact Form
+    var form = $('.contact__form'),
+        message = $('.contact__msg'),
+        form_data;
+    // success function
+    function done_func(response) {
+        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+        message.text(response);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+        form.find('input:not([type="submit"]), textarea').val('');
+    }
+    // fail function
+    function fail_func(data) {
+        message.fadeIn().removeClass('alert-success').addClass('alert-success');
+        message.text(data.responseText);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+    }
+    form.submit(function (e) {
+        e.preventDefault();
+        form_data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: form_data
+        })
+        .done(done_func)
+        .fail(fail_func);
+    });
+    
